68baf46957b6577a5afaceee7d062ffd7a5b1d0f
jmh7186 <jmh7186@gmail.com>
2023-05-11 13:46:04 +0900

    3

---
+++ 1.js
@@ -0,0 +1,24 @@
+window.alert('Yo');
+
+//java, c : 컴파일 언어, 문법이 엄격함
+//javascript, python, vba : 인터프리터 언어(컴파일 X), 문법이 엄격하지 않음
+
+//C언어 : 함수를 기초로 한 언어(객체지향 포함 X)
+//java, javascript : 객체지향
+//객체: 속성+함수
+
+//javascript에서의 객체 확인
+//window.document.태그객체.속성
+//window: 객체
+//document: 객체
+//태그객체: 객체
+//포함관계: window > document > 태그객체
+// window.open() -> 호출
+//window.status -> 값 입력
+
+//window.document.write('Hello');
+// window.document.title = 'HTML 학습';
+
+// window.document.getElementById("태그아이디");
+// window.document.querySelector("div"); -> 모든 div의 태그 선택
+// window.document.querySelector("#id"); -> id 아이디를 가진 객체 선택
